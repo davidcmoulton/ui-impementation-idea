b81760815a86ae755eb2a826da4d185b3a5a69a6
David Moulton <d.moulton@elifesciences.org>
2021-03-12 22:12:46 +0000

    Simplify script

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Wait until the DOM's available
 window.addEventListener('DOMContentLoaded', () => {
-  // Toggle the data attribute to control the menu show / hide when the icon's clicked
-  document.querySelector('#fly-out-toggle').addEventListener('click', (e) => {
-    const pageContainer = document.querySelector('.page-container');
-    const shownFlag = pageContainer.dataset.menuShown;
-    switch (shownFlag) {
-    case 'true':
-      pageContainer.dataset.menuShown = 'false';
-      break;
-    case 'false':
-      pageContainer.dataset.menuShown = 'true';
-      break;
-    case undefined:
-      pageContainer.dataset.menuShown = window.matchMedia('(min-width: 700px)').matches ? 'false' : 'true';
-    }
-  });
+  const pageContainer = document.querySelector('.page-container');
+  if (pageContainer) {
+    const falseIfWideViewport = () => {
+      return window.matchMedia('(min-width: 700px)').matches ? 'false' : 'true';
+    };
+    document.querySelector('#fly-out-toggle').addEventListener('click', () => {
+      if (!pageContainer.dataset.menuShown) {
+        pageContainer.dataset.menuShown = falseIfWideViewport();
+      } else {
+        pageContainer.dataset.menuShown = pageContainer.dataset.menuShown === 'true' ? 'false' : 'true';
+      }
+    });
+  }
 });
